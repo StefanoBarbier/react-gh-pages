16ac49765e8b895a66d49f081e6f9420a67e1ee2
StefanoBarbier <sab388@cornell.edu>
2020-01-08 09:54:22 -0500

    Updates

---
+++ precache-manifest.0419cd19dac86c6843b2bd64ad6cb043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783592a7afbd6816306426c85717f6b9",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7a502ff1bbb913e4bbee",
+    "url": "/react-gh-pages/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "6c1ade2daac9aceb166e",
+    "url": "/react-gh-pages/static/css/main.1e8b1439.chunk.css"
+  },
+  {
+    "revision": "7a502ff1bbb913e4bbee",
+    "url": "/react-gh-pages/static/js/2.8558fc5c.chunk.js"
+  },
+  {
+    "revision": "73c9bbfba730190001ae107e5fde6a6e",
+    "url": "/react-gh-pages/static/js/2.8558fc5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c1ade2daac9aceb166e",
+    "url": "/react-gh-pages/static/js/main.0bbeee5a.chunk.js"
+  },
+  {
+    "revision": "e4ecca00ba951339980c",
+    "url": "/react-gh-pages/static/js/runtime-main.0f5f2be1.js"
+  }
+]);
